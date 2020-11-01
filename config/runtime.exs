9e970c3618c3a3fb3c4bbf0370e54bc8623031ef
q <q@mac2.local>
2020-11-01 20:12:31 +0300

    group config/runtime.exs by module

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,28 +6,16 @@ import Config
 # and secrets from environment variables or elsewhere. Do not define
 # any compile-time configuration in here, as it won't be applied.
 
+#####################################################################
+############################## ENDPOINT #############################
+#####################################################################
+
 config :c, CWeb.Endpoint,
   render_errors: [view: CWeb.ErrorView, accepts: ~w(html json), layout: false],
   pubsub_server: C.PubSub,
   live_view: [signing_salt: "Urm6JRcI"]
 
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
-
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
-
-  config :c, C.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
-
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
       raise """
@@ -48,9 +36,6 @@ if config_env() == :prod do
     ],
     secret_key_base: secret_key_base,
     server: true
-
-  # Do not print debug messages in production
-  config :logger, level: :info
 end
 
 if config_env() == :dev do
@@ -84,7 +69,41 @@ if config_env() == :dev do
         ~r"lib/c_web/templates/.*(eex)$"
       ]
     ]
+end
 
+if config_env() == :test do
+  # We don't run a server during test. If one is required,
+  # you can enable the server option below.
+  config :c, CWeb.Endpoint,
+    secret_key_base: "G3Ln+/DGlLRcc0cFikD44j8AS16t7ab5g0CjqhGBkOz2ol5GjHemYelcDWDEjkw5",
+    url: [host: "localhost"],
+    http: [port: 4002],
+    server: false
+end
+
+#####################################################################
+############################# /ENDPOINT #############################
+#####################################################################
+
+#####################################################################
+################################ REPO ###############################
+#####################################################################
+
+if config_env() == :prod do
+  database_url =
+    System.get_env("DATABASE_URL") ||
+      raise """
+      environment variable DATABASE_URL is missing.
+      For example: ecto://USER:PASS@HOST/DATABASE
+      """
+
+  config :c, C.Repo,
+    # ssl: true,
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+end
+
+if config_env() == :dev do
   # Configure your database
   config :c, C.Repo,
     username: "postgres",
@@ -93,9 +112,6 @@ if config_env() == :dev do
     hostname: "localhost",
     show_sensitive_data_on_connection_error: true,
     pool_size: 10
-
-  # Do not include metadata nor timestamps in development logs
-  config :logger, :console, format: "[$level] $message\n"
 end
 
 if config_env() == :test do
@@ -110,15 +126,35 @@ if config_env() == :test do
     database: "c_test#{System.get_env("MIX_TEST_PARTITION")}",
     hostname: "localhost",
     pool: Ecto.Adapters.SQL.Sandbox
+end
 
-  # We don't run a server during test. If one is required,
-  # you can enable the server option below.
-  config :c, CWeb.Endpoint,
-    secret_key_base: "G3Ln+/DGlLRcc0cFikD44j8AS16t7ab5g0CjqhGBkOz2ol5GjHemYelcDWDEjkw5",
-    url: [host: "localhost"],
-    http: [port: 4002],
-    server: false
+#####################################################################
+############################### /REPO ###############################
+#####################################################################
 
+#####################################################################
+############################### LOGGER ##############################
+#####################################################################
+
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+if config_env() == :prod do
+  # Do not print debug messages in production
+  config :logger, level: :info
+end
+
+if config_env() == :dev do
+  # Do not include metadata nor timestamps in development logs
+  config :logger, :console, format: "[$level] $message\n"
+end
+
+if config_env() == :test do
   # Print only warnings and errors during test
   config :logger, level: :warn
 end
+
+#####################################################################
+############################## /LOGGER ##############################
+#####################################################################
